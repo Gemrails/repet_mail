5416a79a36507a08bcc137a90e3c92096e258095
崔斌 <cuiminmm@gmail.com>
2016-12-07 13:42:42 +0800

    update

--- getmail.py
+++ getmail.py
@@ -69,6 +69,11 @@ class ImapMail(object):
                         self.body = extract_body(msg.get_payload())
                         if _Debug:
                             print usubject
+                            print self.body
+
+                        if not isinstance(self.body, str):
+                            self.body = '.'
+
                         uto = my_unicode(to_me[0][0], to_me[0][1])
 
                         if '上线邮件'.decode(encoding='utf-8') in usubject:
@@ -76,7 +81,6 @@ class ImapMail(object):
                             mail_from = msg['From']
                             Cc = ca.CC
                             sm = SendMail()
-                            print self.body
                             rc = sm._send_mail(mail_from, Cc, usubject, self.body)
                             if _Debug:
                                 print type(mail_from)
